a50c509ffbd27e4bf3e9e72bb09e7ddca08610f4
Shawn Park <shawnpark@10-249-45-76.wireless.oregonstate.edu>
2024-10-24 17:13:46 -0700

    Intro

---
+++ source/Component/Intro.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Intro() {
+    return (
+        <div>
+            <h1>Hi, this is LikeLion OSU Website.</h1>
+        </div>
+    );
+}
+
+export default Intro;
