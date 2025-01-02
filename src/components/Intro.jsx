bfde0585f8aaa94d8c5838ec01003e8bcedc8420
jinnoahlee <167655200+jinnoahlee@users.noreply.github.com>
2025-01-02 11:19:23 -0800

    testing commit 2

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,7 +6,7 @@ function Intro() {
       <h2>Welcome to the Intro Section</h2>
       <ul>
         <li><a href="item1">Item 1</a></li>
-        <li>Item 2</li>
+        <li><a href="item2">Item 2</a></li>
         <li>Item 3</li>
       </ul>
     </div>
