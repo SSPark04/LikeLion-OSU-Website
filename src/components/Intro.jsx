d25d1cc2810d9d70469e0e1b63437fd723272cd4
jinnoahlee <167655200+jinnoahlee@users.noreply.github.com>
2025-01-02 11:05:56 -0800

    Intro.jsx change commit

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,6 +4,11 @@ function Intro() {
   return (
     <div className="intro">
       <h2>Welcome to the Intro Section</h2>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
     </div>
   );
 }
