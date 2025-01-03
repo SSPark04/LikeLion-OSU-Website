07626a9d635af5153199d176257329634a6ac902
jinnoahlee <167655200+jinnoahlee@users.noreply.github.com>
2025-01-03 08:38:43 -0800

    rest of items commit

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 function Intro() {
   return (
     <div className="intro">
-      <h2>Welcome to the Intro Section</h2>
-      <ul>
-        <li><a href="item1">Item 1</a></li>
-        <li><a href="item2">Item 2</a></li>
-        <li>Item 3</li>
-      </ul>
+      <h1><a href="Intro.jsx">LikeLion</a></h1>
+      <nav>
+        <ul>
+          <li><a href="item1">Item 1</a></li>
+          <li><a href="item2">Item 2</a></li>
+          <li><a href="item3">Item 3</a></li>
+        </ul>
+      </nav>
     </div>
   );
 }
